66d9568cce3c256f6e7c7e025f399831dcd2de9c
gKushagra <55290773+gKushagra@users.noreply.github.com>
2021-06-14 17:46:48 -0500

    fixed .env

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  authenticationUrl: "http://api.social-network.fullstackengineers.co.in/auth/",
+  usersUrl: "http://api.social-network.fullstackengineers.co.in/users",
+  contactsUrl: "http://api.social-network.fullstackengineers.co.in/contacts",
+  requestUrl: "http://api.social-network.fullstackengineers.co.in/requests",
+  postsUrl: "http://api.social-network.fullstackengineers.co.in/posts",
+  filesUrl: "http://api.social-network.fullstackengineers.co.in/files",
+  conversationsUrl: "http://api.social-network.fullstackengineers.co.in/conversations",
+  linksUrl: "http://api.social-network.fullstackengineers.co.in/links",
+  callUrl: "http://api.social-network.fullstackengineers.co.in/calls",
+  socketUrl: 'ws://ws.social-network.fullstackengineers.co.in',
+  reconnectInterval: 2000
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -14,7 +14,18 @@ export const environment = {
   linksUrl: "http://localhost:4050/links",
   callUrl: "http://localhost:4050/calls",
   socketUrl: 'ws://localhost:4051',
-  reconnectInterval: 2000
+  reconnectInterval: 2000,
+  // authenticationUrl: "http://api.social-network.fullstackengineers.co.in/auth/",
+  // usersUrl: "http://api.social-network.fullstackengineers.co.in/users",
+  // contactsUrl: "http://api.social-network.fullstackengineers.co.in/contacts",
+  // requestUrl: "http://api.social-network.fullstackengineers.co.in/requests",
+  // postsUrl: "http://api.social-network.fullstackengineers.co.in/posts",
+  // filesUrl: "http://api.social-network.fullstackengineers.co.in/files",
+  // conversationsUrl: "http://api.social-network.fullstackengineers.co.in/conversations",
+  // linksUrl: "http://api.social-network.fullstackengineers.co.in/links",
+  // callUrl: "http://api.social-network.fullstackengineers.co.in/calls",
+  // socketUrl: 'ws://ws.social-network.fullstackengineers.co.in',
+  // reconnectInterval: 2000
 };
 
 /*
